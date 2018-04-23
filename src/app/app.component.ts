1528e4566568a4076778efbc70780ab02b809bd9
Heqian Liu <lhq@bu.edu>
2018-04-23 01:49:41 -0400

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  public stateVar = "login";
+
+
+  public onLoginSuccess() {
+    this.stateVar = "survey";
+  }
 }
